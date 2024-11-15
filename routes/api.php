7c20b02d28125bd1572a4f86d6c4e0390383ab37
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-15 11:11:16 +0530

    user controller added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("auth:sanctum")->group(function(){
     Route::controller(AuthController::class)->prefix("auth")->group(function(){
         Route::get("login","login");
+    });
+
+
+    Route::controller(UserController::class)->prefix("users")->group(function(){
+        Route::get("/","profile");
     });
 });
 
